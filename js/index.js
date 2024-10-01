b91842927dbb3227e3130a30f6592e93e85bb3c5
luckyloo1730 <luckyloo1730@gmail.com>
2024-10-01 19:56:16 +0100

    Website upload

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const pay4fish = document.querySelector(".pay4fish")
+const skydomain = document.querySelector(".skydomain")
+const skystudio = document.querySelector(".skystudio")
+
+pay4fish.addEventListener("click", () => {
+    window.open("https://pay4fish.cloud")
+})
+
+skydomain.addEventListener("click", () => {
+    window.open("https://domains.pay4fish.cloud")
+})
+
+skystudio.addEventListener("click", () => {
+    window.open("https://discord.gg/u5rXXFAeDA")
+})
